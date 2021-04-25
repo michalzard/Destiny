270d7d25f59efd6ac6b216f72a6336b36ed9f3b3
MichalPlatko <miso.platko@gmail.com>
2021-04-26 01:52:53 +0200

    userSchema handles followers,custom methods added(wip)

--- api/schemas/userSchema.js
+++ api/schemas/userSchema.js
@@ -9,8 +9,46 @@ const userSchema=new Schema({
     password:{
         type:String,
         required:[true,"password cannot be blank"],
+    },
+    followers:{
+        followedCount:{
+            type:Number,
+            default:0,
+        },
+        followedBy:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"User", 
+        }
+        ],
+        followingCount:{
+            type:Number,
+            default:0,
+        },
+        following:[
+            {
+            type:Schema.Types.ObjectId,
+            ref:"User",
+            }   
+        ]
     }
-
 })
+
+//TODO: check if userId is already there
+
+userSchema.methods.addFollowedBy=function(userObjectId){
+    if(mongoose.Types.ObjectId.isValid(userObjectId)){
+    this.followers.followedBy.push(userObjectId);
+    this.followers.followedCount++;
+    this.save();
+    }
+}
+userSchema.methods.addFollowing=function(userObjectId){
+    if(mongoose.Types.ObjectId.isValid(userObjectId)
+    && !this.followers.following.equals(userObjectId)){
+    this.followers.following.push(userObjectId);
+    this.save();
+    }
+}
 
 module.exports=mongoose.model("User",userSchema);
