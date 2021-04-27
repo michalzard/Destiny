8df6a6e88c8418f345684a611e416ddbb87caefc
MichalPlatko <miso.platko@gmail.com>
2021-04-27 02:44:12 +0200

    fetches data appropriate to loggedIn user,what is displayed depends on react displayState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,24 @@
-import React from 'react'
-import {useLocation} from "react-router-dom";
-
+import React, { useEffect, useState } from 'react'
 import "../styles/Main.scss";
 import "../styles/main/popover.scss";
-
 import ImageIcon from '@material-ui/icons/Image';
 import GifIcon from '@material-ui/icons/Gif';
 import EmoticonIcon from '@material-ui/icons/InsertEmoticon';
-
-
 import {Button,TextField} from "@material-ui/core";
-
-
-
-
 import LeftSideBar from "./LeftSideBar";
 import RightSideBar from "./RightSidebar";
 import Post from "./Post";
+import axios from 'axios';
 
 
-function Main() {
+function Main({userId}) {
+    const [toDisplay,setToDisplay]=useState("");
+
     return (
         <div className="main">
-        <div className="left"><LeftSideBar/></div>
-        <div className="middle"><Content/></div>
-        <div className="right"><RightSideBar/></div>       
+        <div className="left"><LeftSideBar userId={userId}/></div>
+        <div className="middle"><Content display={toDisplay} setDisplay={setToDisplay} /></div>
+        <div className="right"><RightSideBar userId={userId}/></div>       
         </div>
     )
 }
@@ -34,20 +28,40 @@ export default Main
 
 
 
-function Content(){
-    let location=useLocation();
-    
+function Content({display,setDisplay}){
+    const [allPosts,setPosts]=useState([]);
+    //cost 
+    const getAllPosts=()=>{
+    axios.get("http://localhost:3000/post/all").then(data=>{
+    if(data){
+    const postsArray=data.data.posts;
+    console.log(data.data);
+    for(let i=0;i<postsArray.length;i++){
+        setPosts(prev=>[...prev,postsArray[i]]);
+        
+    }
+    }
+    });
+
+    }
+    useEffect(()=>{
+    getAllPosts();
+    return setPosts([]); //cleanup function
+    },[display]);
     return(
         <div className="content">
             <div>
-            {location.pathname==="/submit" ?  <CreatePostContainer/> :
+            {display==="submit" ?  <CreatePostContainer setDisplay={setDisplay}/> :
             <>
-           <CreatePostRedirect/>
+           <CreatePostRedirect setDisplay={setDisplay}/>
            <div className="content_posts">
-            <Post/><Post/><Post/><Post/>
-            <Post/><Post/><Post/><Post/>
-            <Post/><Post/><Post/><Post/>
-            <Post/><Post/><Post/><Post/>
+            {
+        
+                allPosts.map((post,i)=>{
+                return <Post key={i} author={post.author.username} title={post.title} content={post.content} 
+                votes={post.votes} timestamp={post.createdAt} />
+                })
+            }
             </div>
             </>
             }
@@ -57,30 +71,45 @@ function Content(){
 }
 
 
-function CreatePostRedirect(){
+function CreatePostRedirect({setDisplay}){
     return(
         <div className="content_postHeader">
         <h4>Your Feed</h4>
-        <a href="/submit" style={{textDecoration:"none"}}><Button variant="contained" color="secondary">Create Post</Button></a>
+        <a style={{textDecoration:"none"}}>
+        <Button onClick={()=>{setDisplay("submit");}} variant="contained" color="secondary">Create Post</Button></a>
         </div>
     )
 }
 
 
-function CreatePostContainer(){
+function CreatePostContainer({setDisplay}){
+const [postTitle,setTitle]=useState("");
+const [postText,setText]=useState("");
 const createPost=()=>{
-console.log("post created");
+const userId=localStorage.getItem("user_data");
+if(userId){
+axios.post("http://localhost:3000/post/new",{authorId:userId,title:postTitle,content:postText});
+setTitle("");
+setText("");
+setDisplay("");
+
+}
+}
+const updateTitle=(e)=>{
+    setTitle(e.target.value)
+}
+const updateText=(e)=>{
+    setText(e.target.value)
 }
 return(
-    
     <div className="createPostContainer">
     <h5>Create a post</h5>
     <div className="postInputs">
     <TextField variant="filled" fullWidth className="titleInput"
-    inputProps={{style:{color:"white"}}} 
+    inputProps={{style:{color:"white"}}} onChange={(e)=>{updateTitle(e);}}
     InputLabelProps={{style:{color:"gray"}}} placeholder="Title"></TextField>
     <TextField variant="filled" fullWidth className="textInput"
-    inputProps={{style:{color:"white"}}} 
+    inputProps={{style:{color:"white"}}} onChange={(e)=>{updateText(e);}}
     InputLabelProps={{style:{color:"gray"}}} placeholder="Text(optional)"></TextField>
     </div>
     <div className="postControls">
@@ -95,9 +124,11 @@ return(
     </label>
     <EmoticonIcon className="postIcon"/>
     </div>
-    <div className="postButton"><Button  variant="contained" color="secondary" onClick={()=>{createPost()}}>Post</Button></div>
+    <div className="postButton">
+    <Button  disabled={(postTitle.length>0) ? false : true}
+    variant="contained" color="secondary" onClick={()=>{createPost()}}>Post</Button>
+    </div>
     </div>
     </div>
 )
 }
-
