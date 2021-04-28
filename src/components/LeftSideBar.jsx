16f19fa74122216c0ced2e32a5d214efa89841bf
MichalPlatko <miso.platko@gmail.com>
2021-04-28 12:48:40 +0200

    init

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,11 +1,29 @@
 import "../styles/main/LeftSidebar.scss";
+import {Button} from "@material-ui/core";
 
 function LeftSideBar(){
     return(
-        <div className="leftSidebar">
-        SIDEBAR
+        <div className="leftsidebar">
+        <div className="leftsidebar_logo">Logo gonna be here</div>
+        
+        <div className="leftsidebar_links">
+        <Button color="primary" variant="contained">TEST</Button>
+        <Button color="primary" variant="contained">TEST</Button>
+        <Button color="primary" variant="contained">TEST</Button>
+        <Button color="primary" variant="contained">TEST</Button>
+        <Button color="primary" variant="contained">TEST</Button>
+        <Button color="primary" variant="contained">TEST</Button>
+
+        </div>
+
         </div>
     )
+}
+
+function LinkButton({redirect,text}){
+return(
+    <Button color="primary" variant="contained" href={redirect} >{text}</Button>
+)
 }
 
 export default LeftSideBar;
