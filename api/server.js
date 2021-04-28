8ed9f5c8a86a4cf81a3bf8d8f6c44323c8f5a8b2
MichalPlatko <miso.platko@gmail.com>
2021-04-28 12:49:15 +0200

    formatted

--- api/server.js
+++ api/server.js
@@ -1,28 +1,31 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 require("dotenv").config();
-const port=process.env.BACKEND_PORT || 8080;
-const mongoose=require("mongoose");
-const User=require("./schemas/userSchema");
+const port = process.env.BACKEND_PORT || 8080;
+const mongoose = require("mongoose");
+const User = require("./schemas/userSchema");
 
-const AuthRoute=require("./routes/auth");
-const MemberRoute=require("./routes/members");
-const PostRoute=require("./routes/post");
-const SessionRoute=require("./routes/auth");
+const AuthRoute = require("./routes/auth");
+const MemberRoute = require("./routes/members");
+const PostRoute = require("./routes/post");
+const SessionRoute = require("./routes/auth");
 
 
-mongoose.connect(process.env.DATABASE_URL,
-{useNewUrlParser:true,useUnifiedTopology:true,
-},()=>{console.log("database connected")});
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+}, () => {
+  console.log("database connected")
+});
 
 
 app.use(express.json());
-app.use("/auth",AuthRoute);
-app.use("/m",MemberRoute);
-app.use("/validate",SessionRoute);
-app.use("/post",PostRoute);
+app.use("/auth", AuthRoute);
+app.use("/m", MemberRoute);
+app.use("/validate", SessionRoute);
+app.use("/post", PostRoute);
 
-app.listen(port,()=>console.log(`listening to : ${port}`))
+app.listen(port, () => console.log(`listening to : ${port}`))
 
 //TODO:separate specific routes to its folders
-
