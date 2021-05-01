e1a53ba8087136a25bb37b0dba84380ad31c3e1c
MichalPlatko <miso.platko@gmail.com>
2021-05-01 03:44:14 +0200

    added FullPost view

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,17 +4,18 @@ import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import ShareIcon from '@material-ui/icons/Share';
 import BlockIcon from '@material-ui/icons/Block';
-import {Tooltip,TextField, Button} from "@material-ui/core";
+import {Tooltip,Button,Popover} from "@material-ui/core";
 
 //import FlagIcon from '@material-ui/icons/Flag';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDownSharp';
 import TimeAgo from 'javascript-time-ago'
 // English.
 import en from 'javascript-time-ago/locale/en'
 import { useEffect, useState } from "react";
 import axios from "axios";
-
 TimeAgo.addDefaultLocale(en);
+
 
 function PostOverview({_id,author,title,content,votes,timestamp,borderless=false,borderbottom=false}){
     const [voteDiff,setVoteDiff]=useState(0);
@@ -24,7 +25,17 @@ function PostOverview({_id,author,title,content,votes,timestamp,borderless=false
     const timeAgo=new TimeAgo("en-US");  
     const timestampFormatted=new Date(timestamp).getTime();
     const diff=Date.now() - timestampFormatted;
-    const currentUserID=localStorage.getItem("user_data");
+    const token=localStorage.getItem("token");
+    const [currentUserID,setCurrentUser]=useState('');
+    const fetchUserData=()=>{ 
+    if(token){axios.get(`http://localhost:3001/auth/session?token=${token}`).then(data=>{
+    const member=data.data.user;    
+    if(member){
+    setCurrentUser(member._id);
+    }
+    });
+    }
+    }
     const loadVotes=()=>{
     setVoteDiff(votes.like-votes.dislike);
     }
@@ -68,12 +79,14 @@ function PostOverview({_id,author,title,content,votes,timestamp,borderless=false
         setTimeout(()=>{setShareTooltip("Share")},3000);
         }
     }
+    
     //on post load
     useEffect(()=>{
+    fetchUserData();
     loadVotes();
     alreadyVoted();
     // eslint-disable-next-line
-    },[]);
+    },[currentUserID]);
     return(
     /**content_post adds extra padding between singular posts */
     <div className="content_post">
@@ -116,25 +129,69 @@ function PostOverview({_id,author,title,content,votes,timestamp,borderless=false
  * Display full post even with comments
  */
 function FullPost({_id,author,title,content,votes,timestamp}){
- return(
+const [sort,setSort]=useState("New");
+const [anchorEl, setAnchorEl] = useState(null);
+const [open,setOpen]=useState(false);
+const [id,setId]=useState("");
+
+const [comments,loadComments]=useState([]);
+
+const handlePopOpen=(e)=>{
+ setAnchorEl(e.currentTarget);
+ setOpen(true);
+ setId("simple-popover");
+}
+const handlePopClose=()=>{
+setAnchorEl(null);
+setOpen(false);
+setId(null);
+}
+const handleSortSelect=(e)=>{
+    console.log(e.target);
+    setSort(e.target.innerHTML);
+    handlePopClose();
+}
+return(
      <>
     <PostOverview _id={_id} borderless={true} borderbottom={true}
     author={author} title={title} content={content} votes={votes} timestamp={timestamp} />
-    
     <div className="post_comments">
-    <h5>Comment as author </h5>
-    <TextField placeholder="text" fullWidth/>
-    <Button variant="contained" color="secondary">Comment</Button>
-    <p>sort by "best,new"</p>
-    <p>divider goes here</p>
-    <div className="comment">
-    comment blocks
+    <h6>Comment as <a href={`http://localhost:3000/m/${author._id}`} style={{color:"lightskyblue"}}>{author.username}</a></h6>
+    <div className="add_comment">
+    <textarea className="area" placeholder="What are your thoughts?"></textarea>
+    <Button aria-describedby={id} variant="contained" color="secondary">Comment</Button>
+    </div>
+    <div className="sort_by">Sort by 
+    <div className="sort_info">
+    <span onClick={(e)=>{handlePopOpen(e)}}>
+    <Button variant="text" size="small">{sort}<ArrowDropDownIcon style={{color:"white"}}/></Button> 
+    </span>
+    <Popover id={id} open={open} anchorEl={anchorEl} onClose={handlePopClose} anchorOrigin={{vertical: "bottom",horizontal:"left"}}>
+    <div className="sort_selection">
+    <Button variant="text" onClick={(e)=>{handleSortSelect(e);}}>New</Button>
+    <Button variant="text" onClick={(e)=>{handleSortSelect(e);}}>Best</Button>
+    </div>
+    </Popover>
+    </div>
+    </div>
+    <div className="all_comments">
+    <PostComment/>
     </div> 
     </div>
     </>
  )
 }
 
+function PostComment({author}){
+return(
+    <div className="comment">
+    <div className="info"><span className="post_by">Posted by</span>
+    <span className="author"><a href={`m/author_id`}>m/Test</a></span>
+    <span className="timestamp">666hrs ago</span>
+    </div>
+    </div>
+)
+}
 
 function PostNotFound(){
     return(
